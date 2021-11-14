d15ab18a84174f34a95856e030fa9e888ce00ce8
elenabissocoli03 <91657260+elenabissocoli03@users.noreply.github.com>
2021-11-14 17:17:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+int main(){
+    int n =0, primo = 0, secondo = 1, terzo;
+    cin >> n;
 
-int main() {
-   return 0;
+    if(n < 2){
+        cout << "errore" << endl;
+    } else {
+        cout << "1" << endl;
+            for (int i = 0; i<=(n-2); i++){
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << terzo << endl;
+            }
+    }
+    return 0;
 }
